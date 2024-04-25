81e83eadb4b66e3d05a5a2c4b68b4653ede36e2c
harikairos <167955475+harikairos@users.noreply.github.com>
2024-04-24 21:49:17 -0700

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,1 @@
-#!/usr/bin/env python3
-
-# This is a simple Python script
-# It prints "Hello, world!" to the console
-
-def main():
-	print("Hello, world!")
-	
-if __name__ == "__main__":
-	main()
+print ("Hello, world!")
